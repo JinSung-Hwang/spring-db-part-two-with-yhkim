27037db75dfc2c00791549eca00295494749dd58
JinSung-Hwang <wlstjd7782@gmail.com>
2024-03-26 23:27:39 +0900

    feature: 섹션4 데이터 접근 기술 MyBatis 적용2 - 설정과 실행

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-@Mapper // note: @Mapper가 있어야 MyBatis가 인식할 수 있다. 구현체는 MyBatis가 만들어준다.
+@Mapper // note: @Mapper가 있어야 MyBatis가 인식할 수 있다. 구현체는 MyBatis가 만들어준다. 만들어진 객체는 Bean등록을 진행한다. 따라서 ItemMapper는 주입받아서 사용할 수 있다.
 public interface ItemMapper {
   void save(Item item);
   void update(@Param("id") Long i, @Param("updateParam")ItemUpdateDto updateParam);
